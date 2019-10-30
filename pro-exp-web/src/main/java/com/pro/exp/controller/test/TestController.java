237d380355f6941d9fc8aa8f281c9d0450064650
jingteng <jingteng@zk-house.com>
2019-10-30 17:31:50 +0800

    添加配置文件从服务器读取----暂时

--- pro-exp-web/src/main/java/com/pro/exp/controller/test/TestController.java
+++ pro-exp-web/src/main/java/com/pro/exp/controller/test/TestController.java
@@ -1,6 +1,7 @@
 package com.pro.exp.controller.test;
 
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -13,6 +14,8 @@ import java.util.Map;
 @Api(value = "申请类型管理", tags = "基础信息-申请-申请类型管理", description = "申请类型管理")
 @RequestMapping("/api")
 public class TestController {
+    @Value("${test}")
+    private String testjingteng;
 
     @GetMapping("/test")
     public String testMethod(){
@@ -23,6 +26,7 @@ public class TestController {
     @PostMapping("/post/test")
     public String testPostMethod(@RequestBody Map map){
         System.out.println("请求到达");
+        System.out.println(testjingteng);
         return "test success";
     }
 }
